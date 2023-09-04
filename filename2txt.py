d52e4003ac6ca3fbf32de1e1a009d7785028437a
ShugoYamashita <88839147+ShugoYamashita@users.noreply.github.com>
2023-09-04 14:08:43 +0900

    Update filename2txt.py

--- filename2txt.py
+++ filename2txt.py
@@ -1,13 +1,14 @@
 import os
 
-def write_file_names(directory, output_file):
+def write_file_names(directory_path, output_file):
     with open(output_file, 'w') as file:
-        for filename in os.listdir(directory):
-            file.write(filename + '\n')
+        for filename in os.listdir(directory_path):
+            file_path = os.path.join(directory_path, filename)
+            file.write(file_path + '\n')
 
 def main():
     # ディレクトリパスと出力ファイルパスを指定
-    directory_path = './data/train/input'
+    directory_path = './raindrop/input'
     output_file_path = './output.txt'
 
     # ファイル名を出力
