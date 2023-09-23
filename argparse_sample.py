8b2d7ded157e82d9b60dcb8669caf8b86c2bab70
ShugoYamashita <88839147+ShugoYamashita@users.noreply.github.com>
2023-09-23 21:57:57 +0900

    Update argparse_sample.py

--- argparse_sample.py
+++ argparse_sample.py
@@ -1,16 +1,23 @@
 import argparse
+from pathlib import Path
 
-parser = argparse.ArgumentParser(description='Hyper-parameters for network')
+def get_args_parser():
+    parser = argparse.ArgumentParser(description='Hyper-parameters for network')
+    parser.add_argument('-batch_size', default=32, type=int)
+    parser.add_argument('-seed', help='set random seed', default=0, type=int)
+    parser.add_argument('-pretrained', help='use pretrained weights', action='store_true')
+    parser.add_argument('-output_dir', default='./output_dir',
+                        help='path where to save, empty for no saving')
 
-parser.add_argument('-seed', help='set random seed', default=19, type=int)
-parser.add_argument('-pretrained_mixmae', help='use pretrained weights', action='store_true')
-parser.add_argument('-fix_pretrained_weights', help='fix pretrained weights', action='store_true')
+    return parser
 
-args = parser.parse_args()
+def main(args):
+    if args.pretrained:
+        print('argparse: ', args.pretrained)
 
-seed = args.seed
-print(args.pretrained)
-print(args.fix_pretrained_weights)
-
-if args.pretrained:
-    print('argparse: ', args.pretrained)
+if __name__ == '__main__':
+    args = get_args_parser()
+    args = args.parse_args()
+    if args.output_dir:
+        Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+    main(args)
