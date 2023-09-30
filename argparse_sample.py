135ddd8fa588b9fcc15da9b4a43b09d0f37ef363
ShugoYamashita <88839147+ShugoYamashita@users.noreply.github.com>
2023-09-30 12:49:45 +0900

    Update argparse_sample.py

--- argparse_sample.py
+++ argparse_sample.py
@@ -4,6 +4,7 @@ from pathlib import Path
 def get_args_parser():
     parser = argparse.ArgumentParser(description='Hyper-parameters for network')
     parser.add_argument('-batch_size', default=32, type=int)
+    parser.add_argument('-crop_size', help='Set the crop_size', default=[256, 256], nargs='+', type=int)
     parser.add_argument('-seed', help='set random seed', default=0, type=int)
     parser.add_argument('-pretrained', help='use pretrained weights', action='store_true')
     parser.add_argument('-output_dir', default='./output_dir',
